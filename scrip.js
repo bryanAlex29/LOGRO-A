2b0b737fda683382305b0399d18d4ae04a52c5a0
Alejandro <j.ale.z.m.2004@gmail.com>
2025-05-16 15:36:59 -0500

    2

---
+++ scrip.js
@@ -0,0 +1,4 @@
+  localStorage.setItem("titulo", document.getElementById("titulo").value);
+     localStorage.setItem("titulo", document.getElementById("fecha").value);
+      localStorage.setItem("textarea", document.getElementById("textarea").value);
+    console.log('gurdado con exito')
