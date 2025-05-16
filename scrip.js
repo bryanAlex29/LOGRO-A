de9631cbd4d7de783337716432f509b841d1858d
Bryan Vera <alex29vera.29@gmail.com>
2025-05-16 15:42:32 -0500

    commit de guardar

---
+++ scrip.js
@@ -0,0 +1,24 @@
+
+
+function guardar() {
+    // Obtener los valores de los campos de texto
+    debugger
+    var titulo = document.getElementById("TITULO").value;
+    var descripcion = document.getElementById("DESCRPCION").value;
+    var fecha = document.getElementById("FECHA").value;
+
+    // Crear un objeto con los datos
+    var noticia = {
+        titulo: titulo,
+        descripcion: descripcion,
+        fecha: fecha
+    };
+
+    // Guardar el objeto en el almacenamiento local
+    localStorage.setItem("noticia", JSON.stringify(noticia));
+
+    // Limpiar los campos de texto
+    alert("Noticia guardada exitosamente");
+}
+    
+
